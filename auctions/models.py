64b195b529c8470dea899604428c5bd88be7965f
s13g <ayflix0@gmail.com>
2022-09-22 23:44:18 +0100

    remodelled models

--- auctions/models.py
+++ auctions/models.py
@@ -3,14 +3,12 @@ from django.contrib.auth.models import AbstractUser
 from django.core.validators import MinValueValidator
 from django.db import models
 
+import uuid
+
 
 class User(AbstractUser):
     pass
 
-
-class ItemImage(models.Model):
-    image = CloudinaryField('image', null=True)
-    
 
 class AuctionCategory(models.Model):
     name = models.CharField(max_length=255)
@@ -24,12 +22,15 @@ class AuctionCategory(models.Model):
     
 
 class AuctionItem(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
     item_name = models.CharField(max_length=255)
-    image = models.ForeignKey(ItemImage, on_delete=models.CASCADE, null=True)
+    image = CloudinaryField('image', null=True)
+    category = models.ForeignKey(AuctionCategory, on_delete=models.CASCADE, null=True)
     description = models.TextField()
     watchlist = models.BooleanField(default=False)
     price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(1)])
     listed_by = models.CharField(max_length=255)
+    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
         return f"{self.item_name}"
@@ -44,8 +45,11 @@ class AuctionBid(models.Model):
 
 
 class Comment(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    auction_item = models.ForeignKey(AuctionItem, on_delete=models.CASCADE)
     comment = models.TextField()
     author = models.CharField(max_length=255)
+    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
-        return f"{self.comment}"
+        return f"{self.auction_item} - {self.author}"
