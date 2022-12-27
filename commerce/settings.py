61790a722b8f8394c4cf7bf0786ce8aa7a18fc6d
s13g <ayflix0@gmail.com>
2022-12-27 18:21:30 +0100

    Update ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS with site URL

--- commerce/settings.py
+++ commerce/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://web-production-b183.up.railway.app', '127.0.0.1']
+ALLOWED_HOSTS = ['web-production-b183.up.railway.app', '127.0.0.1']
 
 # Application definition
 
