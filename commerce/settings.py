ccb50dc4f0242112417651f01d08d3c7eec9de50
s13g <ayflix0@gmail.com>
2022-12-28 09:05:02 +0100

    jazzmin config

--- commerce/settings.py
+++ commerce/settings.py
@@ -136,3 +136,94 @@ MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+JAZZMIN_SETTINGS = {
+    # title of the window (Will default to current_admin_site.site_title if absent or None)
+    "site_title": "SUBASTA ADMIN",
+
+    # Title on the login screen (19 chars max) (defaults to current_admin_site.site_header if absent or None)
+    "site_header": "SUBASTA",
+
+    "site_brand": "Subasta Admin",
+
+    # Logo to use for your site, must be present in static files, used for brand on top left ok
+    "site_logo": "assets/images/header-logo.png",
+
+    # Logo to use for your site, must be present in static files, used for login form logo (defaults to site_logo)
+    "login_logo": "assets/images/header-logo.png",
+
+    # CSS classes that are applied to the logo above
+    "site_logo_classes": "img-circle",
+
+    # Relative path to a favicon for your site, will default to site_logo if absent (ideally 32x32 px)
+    "site_icon": None,
+
+    # Welcome text on the login screen
+    "welcome_sign": "Welcome to the Subasta Admin Section",
+
+    # Copyright on the footer
+    "copyright": "Subasta Ltd",
+
+    # The model admin to search from the search bar, search bar omitted if excluded
+    "search_model": "auctions.AuctionItem",
+
+    # Field name on user model that contains avatar ImageField/URLField/Charfield or a callable that receives the user
+    "user_avatar": "avatar",
+
+    ############
+    # Top Menu #
+    ############
+
+    # Links to put along the top menu
+    "topmenu_links": [
+
+        # Url that gets reversed (Permissions can be added)
+        {"name": "Home", "url": "admin:index", "permissions": ["auth.view_user"]},
+
+        # model admin to link to (Permissions checked against model)
+        # {"model": "accounts.User"},
+
+        # App with dropdown menu to all its models pages (Permissions checked against models)
+        {"app": "users"},
+        {"app": "auctions"},
+    ],
+
+    #############
+    # User Menu #
+    #############
+
+    # Additional links to include in the user menu on the top right ("app" url type is not allowed)
+    "usermenu_links": [
+        {"name": "Subasta Platform", "url": "/", "new_window": True},
+        {"model": "auth.user"}
+    ],
+
+    # List of apps (and/or models) to base side menu ordering off of (does not need to contain all apps/models)
+    # "order_with_respect_to": ["auth", "accounts", "accounts.user", "accounts.tutor", "accounts.student", "lessons"],
+
+    # Custom icons for side menu apps/models See https://fontawesome.com/icons?d=gallery&m=free&v=5.0.0,5.0.1,5.0.10,5.0.11,5.0.12,5.0.13,5.0.2,5.0.3,5.0.4,5.0.5,5.0.6,5.0.7,5.0.8,5.0.9,5.1.0,5.1.1,5.2.0,5.3.0,5.3.1,5.4.0,5.4.1,5.4.2,5.13.0,5.12.0,5.11.2,5.11.1,5.10.0,5.9.0,5.8.2,5.8.1,5.7.2,5.7.1,5.7.0,5.6.3,5.5.0,5.4.2
+    # for the full list of 5.13.0 free icon classes
+    "icons": {
+        "auth.group": "fas fa-users",
+        "users.user": "fas fa-user-cog",
+        "auctions.comment": "fas fa-comments",
+        "auctions.auctionitem": "fas fa-list",
+        "auctions.auctionbid": "fas fa-money-bill",
+        "auctions.category": "fas fa-tag",
+        "auctions.watchlist": "fas fa-eye",
+
+    },
+    # Icons that are used when one is not manually specified
+    "default_icon_parents": "fas fa-chevron-circle-right",
+    "default_icon_children": "fas fa-circle",
+
+    #############
+    # UI Tweaks #
+    #############
+
+    "show_ui_builder": False,
+
+    "changeform_format": "horizontal_tabs",
+    # override change forms on a per modeladmin basis
+    "changeform_format_overrides": {"accounts.user": "collapsible", "accounts.group": "vertical_tabs"},
+}
