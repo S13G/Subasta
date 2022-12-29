2dfd2b6db3f32a11a48679faf93f128c3d984bac
s13g <ayflix0@gmail.com>
2022-12-29 19:13:09 +0100

    search query

--- commerce/settings.py
+++ commerce/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['web-production-b183.up.railway.app', '127.0.0.1']
 
