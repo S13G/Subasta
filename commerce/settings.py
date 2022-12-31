7b5bf6d1e42205340644ebae2c68b3a86c6a8e0c
s13g <ayflix0@gmail.com>
2022-12-31 23:01:14 +0100

    deployment

--- commerce/settings.py
+++ commerce/settings.py
@@ -142,7 +142,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
 
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.CachedStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
